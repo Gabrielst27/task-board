137836df8167550dbe675f3aa0d8ecad4f8a7358
Gabriel Torres <gabrielst328@gmail.com>
2025-05-06 15:34:50 -0300

    feat: layout module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutModule } from './ui/layout/layout.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
